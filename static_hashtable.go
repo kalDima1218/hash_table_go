fc8325044b877614c31be2c8b437cf8fbad5a0c0
Dmitry Kalugin <kalugin180806@gmail.com>
2023-03-25 00:01:30 +0300

    Update static_hashtable.go

--- static_hashtable.go
+++ static_hashtable.go
@@ -6,21 +6,30 @@ import (
 	"math/rand"
 )
 
+type Item interface {
+	hash()int
+}
+
 type HashTable struct{
 	load, size int
-	val []int
+	val []Item
 	t []int
 }
 
 func newHashTable() HashTable {
 	var tmp HashTable
 	tmp.size, tmp.load = 3, 0
-	tmp.val, tmp.t = make([]int, 3), make([]int, 3)
+	tmp.val = make([]Item, 3)
+	tmp.t = make([]int, 3)
 	return tmp
 }
 
-func hash(x int) int{
-	return int(math.Abs(float64(x)))
+func _newHashTableSized(size int) HashTable {
+	var tmp HashTable
+	tmp.size, tmp.load = size, 0
+	tmp.val = make([]Item, size)
+	tmp.t = make([]int, size)
+	return tmp
 }
 
 func h1(x, size int) int{
@@ -31,12 +40,12 @@ func h2(x, size int) int{
 	return 1 + x % (size - 2)
 }
 
-func (mp *HashTable) insert(x int){
+func (mp *HashTable) insert(x Item){
 	if float64(mp.load) / float64(mp.size) >= 0.5{
-		mp.rehash()
+		mp._rehash()
 	}
-	var i = h1(hash(x), mp.size)
-	var d = h2(hash(x), mp.size)
+	var i = h1(x.hash(), mp.size)
+	var d = h2(x.hash(), mp.size)
 	for mp.t[i] > 0{
 		if mp.val[i] == x{
 			mp.t[i]+=1
@@ -49,9 +58,9 @@ func (mp *HashTable) insert(x int){
 	mp.load++
 }
 
-func (mp *HashTable) count(x int) int{
-	var i = h1(hash(x), mp.size)
-	var d = h2(hash(x), mp.size)
+func (mp *HashTable) count(x Item) int{
+	var i = h1(x.hash(), mp.size)
+	var d = h2(x.hash(), mp.size)
 	for mp.t[i] > 0{
 		if mp.val[i] == x{
 			return mp.t[i]
@@ -61,10 +70,15 @@ func (mp *HashTable) count(x int) int{
 	return 0
 }
 
-func (mp *HashTable) rehash(){
+func (mp *HashTable) clear(){
+	*mp = newHashTable()
+}
+
+func (mp *HashTable) _rehash(){
 	var _mp HashTable
 	_mp.size, _mp.load = mp.size * 2, 0
-	_mp.val, _mp.t = make([]int, mp.size * 2), make([]int, _mp.size * 2)
+	_mp.val = make([]Item, mp.size * 2)
+	_mp.t = make([]int, _mp.size * 2)
 	for i := 0; i < mp.size; i++{
 		for j := 0; j < mp.t[i]; j++{
 			_mp.insert(mp.val[i])
@@ -73,15 +87,62 @@ func (mp *HashTable) rehash(){
 	*mp = _mp
 }
 
+
+
+type ItemInt struct{
+	val int
+}
+
+func (i ItemInt)hash() int{
+	return int(math.Abs(float64(i.val)))
+}
+
+func newItemInt(x int) ItemInt{
+	var tmp ItemInt
+	tmp.val = x
+	return tmp
+}
+
+type ItemString struct{
+	val string
+}
+
+func (i ItemString)hash() int{
+	const b, m = 131, 1000000007
+	var h = 0
+	for _, c := range i.val{
+		h*=b
+		h%=m
+		h+=int(c+1)
+		h%=m
+	}
+	return h
+}
+
+func newItemString(x string) ItemString{
+	var tmp ItemString
+	tmp.val = x
+	return tmp
+}
+
 func main() {
 	var mp HashTable
 	mp = newHashTable()
+
+	mp.insert(newItemString("aba"))
+	mp.insert(newItemString("caba"))
+	fmt.Println("aba", mp.count(newItemString("aba")))
+	fmt.Println("abac", mp.count(newItemString("abac")))
+	fmt.Println("caba", mp.count(newItemString("caba")))
+
+	mp.clear()
+
 	for i := 0; i < 10; i++{
-		mp.insert(rand.Int()%10)
+		mp.insert(newItemInt(rand.Int()%10))
 	}
 	for i := 0; i < 10; i++{
-		if mp.count(i) > 0{
-			fmt.Println(i, mp.count(i))
+		if mp.count(newItemInt(i)) > 0{
+			fmt.Println(i, mp.count(newItemInt(i)))
 		}
 	}
 }
